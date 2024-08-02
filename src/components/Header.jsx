b7c398d698cd8cf6992e16d1f95f5b9ee1262834
avicos <adapaavinash612@gmail.com>
2024-08-02 14:09:03 +0530

    add next-auth and modify the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { signIn, useSession, signOut } from 'next-auth/react';
+
 
 export default function Header() {
+  const {data: session} = useSession();
+
   return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
         <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -27,7 +33,19 @@ export default function Header() {
             className='bg-gray-50 border border-gray-200 rounded text-sm w-full
              py-2 px-4 max-w-[210px]' />
 
-             <button className='text-sm font-semibold text-blue-500'>Log In</button>
+            {session ? (
+
+              <img src={session.user.image} alt={session.user.name}
+              className='h-10 w-10 rounded-full cursor-pointer'
+              onClick={signOut} />
+
+            ) : (
+              <button className='text-sm font-semibold text-blue-500'
+             onClick={signIn}>Log In</button>
+            )}
+             
+
+             
         </div>
     </div>
   )
